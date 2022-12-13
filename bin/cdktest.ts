e0232f83f13f82125b1506a6db8f490745a7b82c
Alain Martin <alain@speedyrails.com>
2022-12-13 09:53:14 -0300

    First change

---
+++ bin/cdktest.ts
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from 'aws-cdk-lib';
+import { AwsCodepipelineProjectStack } from '../lib/cdktest-stack';
+
+const app = new cdk.App();
+new AwsCodepipelineProjectStack(app, 'AwsCodepipelineProjectStack', {
+
+  // to create a new isntance of the class AwsCdkCodePipelineStack. This class is defined in lib directory
+  // it is this section wehere we can all the details of the cdk pipeline
+  
+  env: {
+    account: "2703577448385",      // replace with your aws account ID
+    region: "us-west-1",         // replace with your preferred region
+  },
+
+});
