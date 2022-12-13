af0bc4c495b6e8f50c28c9da0758df1ce437da79
Alain Martin <alain@speedyrails.com>
2022-12-13 10:29:07 -0300

    Second change//

--- bin/cdktest.ts
+++ bin/cdktest.ts
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
 
-#!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { AwsCodepipelineProjectStack } from '../lib/cdktest-stack';
@@ -12,7 +11,7 @@ new AwsCodepipelineProjectStack(app, 'AwsCodepipelineProjectStack', {
   // it is this section wehere we can all the details of the cdk pipeline
   
   env: {
-    account: "2703577448385",      // replace with your aws account ID
+    account: "703577448385",      // replace with your aws account ID
     region: "us-west-1",         // replace with your preferred region
   },
 
